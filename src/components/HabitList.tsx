33515a3b4884453dbf01a37a56dd0b112892cac6
Stefan Turdean <Stefan.Turdean@gmail.com>
2026-09-18 23:53:42 +0300

    added: button, habitList, habitForm, header

---
+++ src/components/HabitList.tsx
@@ -0,0 +1,55 @@
+import { Button } from "./Button";
+
+export function HabitList() {
+  const habits = [
+    { id: "1", name: "hi" },
+    { id: "2", name: "hi2" },
+  ];
+
+  if (habits.length === 0) {
+    return (
+      <p className="text-center text-zinc-500 py-12">
+        No habit yet. Add one to get started
+      </p>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      {habits.map((habit) => (
+        <HabitItem key={habit.id} habit={habit} />
+      ))}
+    </div>
+  );
+}
+
+type HabitItemProps = {
+  habit: {
+    id: string;
+    name: string;
+  };
+};
+
+function HabitItem({ habit }: HabitItemProps) {
+  const visibleDates = [new Date()];
+
+  return (
+    <div className="rounded-xl bg-zinc-800 p-4 flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <div className="flex gap-3 items-center">
+          <span className="font-medium">{habit.name}</span>
+          <span className="text-sm text-amber-400">3</span>
+        </div>
+        <Button>DELETE</Button>
+      </div>
+      <div className="flex gap-1.5">
+        {visibleDates.map((date) => (
+          <Button key={date.toDateString()}>
+            <span className="font-medium">mon</span>
+            <span>2</span>
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
